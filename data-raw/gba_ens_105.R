282b8c56f38b7a12a98922ba50e25ca4f89ae224
David Zhang <dyzhang32@gmail.com>
2022-01-31 16:42:22 +0000

    feat: add gba data for examples and tests

---
+++ data-raw/gba_ens_105.R
@@ -0,0 +1,42 @@
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(rtracklayer)
+library(R.utils)
+
+# Main --------------------------------------------------------------------
+
+gtf_path <- file.path(tempdir(), "Homo_sapiens.GRCh38.105.chr.gtf.gz")
+
+# download ens 105 gtf
+download.file(
+    stringr::str_c(
+      "http://ftp.ensembl.org/pub/release-105/gtf/homo_sapiens/",
+      "Homo_sapiens.GRCh38.105.chr.gtf.gz"
+      ),
+    destfile = gtf_path
+)
+
+# unzip gtf
+R.utils::gunzip(gtf_path)
+
+gtf_path <- gtf_path %>%
+    stringr::str_remove("\\.gz$")
+
+gtf <- rtracklayer::import(gtf_path)
+
+# extract gba transcripts
+# convert to data.frame()
+gba_ens_105 <-
+    gtf[!is.na(gtf$gene_name) & gtf$gene_name == "GBA"] %>%
+    as.data.frame() %>%
+    dplyr::as_tibble()
+
+# Save data ---------------------------------------------------------------
+
+usethis::use_data(
+    gba_ens_105,
+    compress = "gzip",
+    overwrite = TRUE
+)
