dd851ace025f6bf19aa24149525ffaf78fe2aa64
David Zhang <dyzhang32@gmail.com>
2022-02-03 20:41:34 +0000

    refactor(GeomRange): inherit from GeomTile, rather than GeomRect

--- R/geom_range.R
+++ R/geom_range.R
@@ -22,13 +22,8 @@ geom_range <- function(mapping = NULL, data = NULL,
 }
 
 #' @noRd
-GeomRange <- ggplot2::ggproto("GeomRange", ggplot2::GeomRect,
-
-    # TODO - check whether this is appropriate
-    extra_params = c("na.rm"),
+GeomRange <- ggplot2::ggproto("GeomRange", ggplot2::GeomTile,
     setup_data = function(data, params) {
-        browser()
-
         data$height <- data$height %||% params$height %||% 0.5
 
         transform(data,
@@ -44,10 +39,5 @@ GeomRange <- ggplot2::ggproto("GeomRange", ggplot2::GeomRect,
         size = 0.1, linetype = 1,
         alpha = NA, height = NA
     ),
-    required_aes = c("x_start", "x_end", "y"),
-
-    # Blindly copied from GeomTile, justification:
-    # https://github.com/tidyverse/ggplot2/blob/c89c265a57fd71f8a0288ce81037296aadc0a012/R/geom-tile.r#L116-L118
-    non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
-    draw_key = ggplot2::draw_key_polygon
+    required_aes = c("x_start", "x_end", "y")
 )
