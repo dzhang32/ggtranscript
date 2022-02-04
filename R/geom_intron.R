5baec8c4373af88f70c616dfe73cf08b78e1e690
David Zhang <dyzhang32@gmail.com>
2022-02-04 18:21:40 +0000

    fix: add back to_intron (accidentally deleted)

--- R/geom_intron.R
+++ R/geom_intron.R
@@ -93,3 +93,31 @@ GeomIntron <- ggplot2::ggproto("GeomIntron", ggplot2::GeomSegment,
         )
     }
 )
+
+#' @noRd
+to_intron <- function(x, group_var = NULL, start_var = start, end_var = end) {
+
+    # grouping by NULL (default) does nothing
+    x <- x %>%
+        dplyr::group_by({{ group_var }})
+
+    # make sure exons are arranged by coord, so that dplyr::lag works correctly
+    x <- x %>%
+        dplyr::arrange({{ start_var }}, {{ end_var }})
+
+    # obtain intron start and ends
+    x <- x %>%
+        dplyr::mutate(
+            intron_start := dplyr::lag({{ end_var }}) + 1,
+            intron_end := {{ start_var }} - 1
+        ) %>%
+        dplyr::select(-{{ start_var }}, -{{ end_var }})
+
+    # for each group, output N of introns should be N - 1 the inputted exons
+    # remove the introduced artifact NAs
+    x <- x %>%
+        dplyr::ungroup() %>%
+        dplyr::filter(!is.na(intron_start) & !is.na(intron_end))
+
+    return(x)
+}
