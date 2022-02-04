541c54cfdbd148a7e1b14da7a6085682f618eee6
David Zhang <dyzhang32@gmail.com>
2022-02-04 18:24:25 +0000

    build: create globals.R to bypass R CMD Check note

---
+++ R/globals.R
@@ -0,0 +1,9 @@
+# bypass R CMD Check notes, related to tidyverse non-standard evaluation
+# https://www.r-bloggers.com/2019/08/no-visible-binding-for-global-variable/
+utils::globalVariables(c(
+    "start",
+    "end",
+    ":=",
+    "intron_start",
+    "intron_end"
+))
