aaadf47b55420abbbb1833b69de3c0207fcf770a
David Zhang <dyzhang32@gmail.com>
2022-02-05 15:01:18 +0000

    test(geom_range): add tests for geom_range

--- tests/testthat/test-geom_range.R
+++ tests/testthat/test-geom_range.R
@@ -1,15 +1,50 @@
-# library(tidyverse)
-#
-# gba_ens_105 %>%
-#   dplyr::filter(type == "exon") %>%
-#   ggplot() +
-#   geom_range(
-#     aes(
-#       xstart = start,
-#       xend = end,
-#       y = transcript_name,
-#       fill = transcript_name,
-#       height = 1
-#     ),
-#     colour = "black"
-#   )
+# create dummy exons for testing
+test_exons <-
+    dplyr::tibble(
+        start = c(100, 300, 500, 650),
+        end = start + 100,
+        strand = c("+", "+", "-", "-"),
+        tx = c("A", "A", "B", "B")
+    )
+
+# create base plot to be used in downstream tests
+test_exons_plot <- test_exons %>%
+    ggplot2::ggplot(aes(
+        xstart = start,
+        xend = end,
+        y = tx
+    ))
+
+##### geom_range #####
+
+testthat::test_that(
+    "geom_range() works correctly",
+    {
+        base_geom_range <- test_exons_plot +
+            geom_range()
+        w_param_geom_range <- test_exons_plot +
+            geom_range(colour = "red", fill = "blue")
+        w_aes_geom_range <- test_exons_plot +
+            geom_range(aes(fill = tx))
+        w_facet_geom_range <- test_exons_plot +
+            geom_range() +
+            ggplot2::facet_wrap(~tx)
+
+        vdiffr::expect_doppelganger(
+            "Base geom_range plot",
+            geom_range
+        )
+        vdiffr::expect_doppelganger(
+            "With param geom_range plot",
+            w_param_geom_range
+        )
+        vdiffr::expect_doppelganger(
+            "With aes geom_range plot",
+            w_aes_geom_range
+        )
+        vdiffr::expect_doppelganger(
+            "With facet geom_range plot",
+            w_facet_geom_range
+        )
+    }
+)
