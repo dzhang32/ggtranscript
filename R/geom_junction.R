1f8e1d86766e8033c2a6e41218cea0c83a6d5604
David Zhang <dyzhang32@gmail.com>
2022-02-05 15:20:43 +0000

    feat: create geom_junction

---
+++ R/geom_junction.R
@@ -0,0 +1,66 @@
+#' @noRd
+geom_junction <- function(mapping = NULL, data = NULL,
+                          stat = "identity", position = "identity",
+                          ...,
+                          curvature = -0.5,
+                          angle = 90,
+                          ncp = 5,
+                          arrow = NULL,
+                          arrow.fill = NULL,
+                          lineend = "butt",
+                          na.rm = FALSE,
+                          show.legend = NA,
+                          inherit.aes = TRUE) {
+    ggplot2::layer(
+        data = data,
+        mapping = mapping,
+        stat = stat,
+        geom = GeomJunction,
+        position = position,
+        show.legend = show.legend,
+        inherit.aes = inherit.aes,
+        params = list(
+            arrow = arrow,
+            arrow.fill = arrow.fill,
+            curvature = curvature,
+            angle = angle,
+            ncp = ncp,
+            lineend = lineend,
+            na.rm = na.rm,
+            ...
+        )
+    )
+}
+
+#' @noRd
+GeomJunction <- ggplot2::ggproto("GeomJunction", ggplot2::GeomCurve,
+    required_aes = c("xstart", "xend", "y"),
+    setup_data = function(data, params) {
+        # needed to permit usage of xstart/xend
+        transform(
+            data,
+            x = xstart,
+            xend = xend,
+            y = y,
+            yend = y,
+            xstart = NULL
+        )
+    },
+
+    # default_aes = aes(colour = "black", size = 0.5, linetype = 1, alpha = NA),
+    draw_panel = function(data, panel_params, coord, curvature = -0.5, angle = 90,
+                          ncp = 5, arrow = NULL, arrow.fill = NULL, lineend = "butt", na.rm = FALSE) {
+        ggplot2::GeomCurve$draw_panel(
+            data = data,
+            panel_params = panel_params,
+            coord = coord,
+            curvature = curvature,
+            angle = angle,
+            ncp = ncp,
+            arrow = arrow,
+            arrow.fill = arrow.fill,
+            lineend = lineend,
+            na.rm = na.rm
+        )
+    }
+)
