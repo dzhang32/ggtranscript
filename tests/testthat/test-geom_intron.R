751e14bc9b007676d9aafaa12b4150ac2f62ba6a
David Zhang <dyzhang32@gmail.com>
2022-02-05 14:02:46 +0000

    test: add test for plots of geom_intron using vdiffr
    - using vdiffr simplifies snapshot tests
    - need to keep vdiffr version the same though, otherwise test is skipped

--- tests/testthat/test-geom_intron.R
+++ tests/testthat/test-geom_intron.R
@@ -3,18 +3,28 @@
 # create dummy exons for testing
 test_exons <-
     dplyr::tibble(
-        start = c(5, 10, 15, 20),
-        end = c(7, 12, 17, 22),
+        start = c(100, 300, 500, 650),
+        end = start + 100,
+        strand = c("+", "+", "-", "-"),
         tx = c("A", "A", "B", "B")
     )
 
 # manually create the expected introns
 test_introns <-
     dplyr::tibble(
+        strand = c("+", "-"),
         tx = c("A", "B"),
-        intron_start = c(8, 18),
-        intron_end = c(9, 19)
+        intron_start = c(201, 601),
+        intron_end = c(299, 649)
     )
+
+# create base plot to be used in downstream tests
+test_introns_plot <- test_introns %>%
+    ggplot2::ggplot(aes(
+        xstart = intron_start,
+        xend = intron_end,
+        y = tx
+    ))
 
 testthat::test_that("to_intron() obtains introns correctly", {
     # with group_var
@@ -48,24 +58,39 @@ testthat::test_that(
 ##### geom_intron #####
 
 testthat::test_that(
+    "geom_intron() works correctly",
+    {
+        base_geom_intron <- test_introns_plot +
+            geom_intron()
+        w_param_geom_intron <- test_introns_plot +
+            geom_intron(colour = "blue", size = 2)
+        w_aes_geom_intron <- test_introns_plot +
+            geom_intron(aes(colour = tx, size = c(1L, 2L)))
+
+        vdiffr::expect_doppelganger(
+            "Base geom_intron plot",
+            base_geom_intron
+        )
+        vdiffr::expect_doppelganger(
+            "With param geom_intron plot",
+            w_param_geom_intron
+        )
+        vdiffr::expect_doppelganger(
+            "With aes geom_intron plot",
+            w_aes_geom_intron
+        )
+    }
+)
+
+testthat::test_that(
     "geom_intron() catches strand input errors",
     {
-        na_strand <- test_introns %>%
-            ggplot2::ggplot() +
-            geom_intron(ggplot2::aes(
-                xstart = intron_start, xend = intron_end,
-                y = tx
-            ),
-            strand = NA
-            )
-        a_strand <- test_introns %>%
-            ggplot2::ggplot() +
-            geom_intron(ggplot2::aes(
-                xstart = intron_start, xend = intron_end,
-                y = tx
-            ),
-            strand = "a"
-            )
+        na_strand <- test_introns_plot +
+            geom_intron(strand = c(NA, rep("+", nrow(test_introns) - 1)))
+        a_strand <- test_introns_plot +
+            geom_intron(strand = "a")
+        int_strand <- test_introns_plot +
+            geom_intron(aes(strand = intron_start))
         # seems to require print to catch error
         expect_error(
             print(na_strand),
@@ -75,28 +100,20 @@ testthat::test_that(
             print(a_strand),
             "strand values must be one of"
         )
+        expect_error(
+            print(int_strand),
+            "strand values must be one of"
+        )
     }
 )
 
 testthat::test_that(
     "geom_intron() catches arrow.min.intron.length input errors",
     {
-        neg_arrow.min <- test_introns %>%
-            ggplot2::ggplot() +
-            geom_intron(ggplot2::aes(
-                xstart = intron_start, xend = intron_end,
-                y = tx
-            ),
-            arrow.min.intron.length = -1
-            )
-        chr_arrow.min <- test_introns %>%
-            ggplot2::ggplot() +
-            geom_intron(ggplot2::aes(
-                xstart = intron_start, xend = intron_end,
-                y = tx
-            ),
-            arrow.min.intron.length = "1"
-            )
+        neg_arrow.min <- test_introns_plot +
+            geom_intron(arrow.min.intron.length = -1)
+        chr_arrow.min <- test_introns_plot +
+            geom_intron(arrow.min.intron.length = "1")
         # seems to require print to catch error
         expect_error(
             print(neg_arrow.min),
@@ -109,12 +126,6 @@ testthat::test_that(
     }
 )
 
-# test_introns %>%
-#   ggplot2::ggplot() +
-#   geom_intron(ggplot2::aes(
-#     xstart = intron_start, xend = intron_end,
-#     y = tx
-#   ),
-#   strand = "+",
-#   arrow.min.intron.length = 0
-#   )
+
+# test_introns_plot +
+#   geom_intron(aes(colour = tx, strand = strand))
