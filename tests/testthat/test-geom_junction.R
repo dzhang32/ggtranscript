2a0c84508a188d40db597d13309ad17597a5d80b
David Zhang <dyzhang32@gmail.com>
2022-02-06 16:57:03 +0000

    test(geom_junction): test using gba_ens_105
    - needs more junctions/txs to see full complexity of junction.orientation

--- tests/testthat/test-geom_junction.R
+++ tests/testthat/test-geom_junction.R
@@ -1,19 +1,16 @@
 # manually create the expected introns
 test_introns <-
-    dplyr::tibble(
-        strand = c("+", "-"),
-        tx = c("A", "B"),
-        start = c(201, 601),
-        end = c(299, 649),
-        type = "intron"
-    )
+    gba_ens_105 %>%
+    dplyr::filter(type == "exon") %>%
+    to_intron(group_var = transcript_name)
+
 
 # create base plot to be used in downstream tests
 test_introns_plot <- test_introns %>%
     ggplot2::ggplot(aes(
         xstart = start,
         xend = end,
-        y = tx
+        y = transcript_name
     ))
 
 ##### geom_junction #####
@@ -24,16 +21,16 @@ testthat::test_that(
         base_geom_junction <- test_introns_plot +
             geom_junction()
         w_param_geom_junction <- test_introns_plot +
-            geom_junction(colour = "red", size = 2)
+            geom_junction(colour = "red", curvature = 0.25)
         w_aes_geom_junction <- test_introns_plot +
-            geom_junction(aes(colour = tx))
+            geom_junction(aes(colour = transcript_name))
         w_facet_geom_junction <- test_introns_plot +
             geom_junction() +
-            ggplot2::facet_wrap(~tx)
+            ggplot2::facet_wrap(~transcript_biotype)
 
         vdiffr::expect_doppelganger(
             "Base geom_junction plot",
-            geom_junction
+            base_geom_junction
         )
         vdiffr::expect_doppelganger(
             "With param geom_junction plot",
@@ -58,7 +55,11 @@ testthat::test_that(
         bottom_junction.orientation <- test_introns_plot +
             geom_junction(junction.orientation = "bottom")
         w_aes_param_top_junction.orientation <- test_introns_plot +
-            geom_junction(aes(colour = tx), size = 1, junction.orientation = "top")
+            geom_junction(
+                aes(colour = transcript_name),
+                size = 1,
+                junction.orientation = "top"
+            )
 
         vdiffr::expect_doppelganger(
             "top junction.orientation plot",
@@ -80,10 +81,16 @@ testthat::test_that(
     {
         a_junction.orientation <- test_introns_plot +
             geom_junction(junction.orientation = "a")
+        neg_curvature_junction.orientation <- test_introns_plot +
+            geom_junction(curvature = -0.5)
 
         expect_error(
             print(a_junction.orientation),
             "junction.orientation must be one of"
+        )
+        expect_warning(
+            print(neg_curvature_junction.orientation),
+            "Setting curvature of < 0 will flip"
         )
     }
 )
