ebd79fa7a2e30c26a2cc0b9b5cb308baa116a7d3
David Zhang <dyzhang32@gmail.com>
2022-02-10 15:18:59 +0000

    refactor(to_intron): match arg names of to_diff()

--- R/to_intron.R
+++ R/to_intron.R
@@ -1,13 +1,14 @@
 #' Convert exon co-ordinates to introns
 #'
-#' Taking a set of exons, `to_intron` will return the corresponding introns.
-#' `to_intron` expects inputted exons to either 1. originate from a single
-#' transcript or 2. be grouped via `group_var` such that each group corresponds
-#' to a single transcript. In other words, `to_intron` assumes that exons (from
-#' each group) do not overlap one another.
+#' Taking as input set of `exons`, `to_intron` will return the corresponding
+#' introns. `to_intron` expects inputted exons to either 1. originate from a
+#' single transcript or 2. be grouped via `group_var` such that each group
+#' corresponds to a single transcript. In other words, `to_intron` assumes that
+#' exons (from each transcript/`group_var`) do not overlap one another.
 #'
-#' Important: this functions defines introns precisely as the exon boundaries,
-#' rather than the intron start/end being exon start - 1, exon end + 1.
+#' Important: for visualisation purposes this functions defines introns
+#' precisely as the exon boundaries, rather than the intron start/end being
+#' (exon end + 1)/(exon start - 1).
 #'
 #' @inheritParams to_diff
 #'
@@ -39,24 +40,24 @@
 #'     geom_intron(
 #'         data = to_intron(example_exons, "tx")
 #'     )
-to_intron <- function(x, group_var = NULL) {
-    .check_coord_object(x)
-    .check_group_var(x, group_var)
+to_intron <- function(exons, group_var = NULL) {
+    .check_coord_object(exons)
+    .check_group_var(exons, group_var)
 
     # TODO - add functionality to check warn if exons overlap
     # as this should could break the function
     # which expects exons in each group_var to originate from a single tx
 
     if (!is.null(group_var)) {
-        x <- x %>% dplyr::group_by_at(.vars = group_var)
+        exons <- exons %>% dplyr::group_by_at(.vars = group_var)
     }
 
     # make sure exons are arranged by coord, so that dplyr::lag works correctly
-    x <- x %>%
+    exons <- exons %>%
         dplyr::arrange(start, end)
 
     # obtain intron start and ends
-    x <- x %>%
+    exons <- exons %>%
         dplyr::mutate(
             intron_start := dplyr::lag(end),
             intron_end := start,
@@ -67,10 +68,10 @@ to_intron <- function(x, group_var = NULL) {
     # for each group, output N of introns should be N - 1 the inputted exons
     # remove the introduced artifact NAs
     # rename intron_start/intron_end to
-    x <- x %>%
+    exons <- exons %>%
         dplyr::ungroup() %>%
         dplyr::filter(!is.na(intron_start) & !is.na(intron_end)) %>%
         dplyr::rename(start = intron_start, end = intron_end)
 
-    return(x)
+    return(exons)
 }
