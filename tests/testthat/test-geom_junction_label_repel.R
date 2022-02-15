674e31bd0a3a8db82c60bb46d78d32aaaf77f52d
David Zhang <dyzhang32@gmail.com>
2022-02-15 22:53:51 +0000

    test(geom_junction_label_repel): add testing (#3)

---
+++ tests/testthat/test-geom_junction_label_repel.R
@@ -0,0 +1,69 @@
+# manually create the expected introns
+test_introns <-
+    gba_ens_105 %>%
+    dplyr::filter(
+        type == "exon",
+        transcript_name %in% c("GBA-201", "GBA-202")
+    ) %>%
+    to_intron(group_var = "transcript_name") %>%
+    dplyr::mutate(
+        count = dplyr::row_number()
+    )
+
+# create base plot to be used in downstream tests
+test_introns_plot <- test_introns %>%
+    ggplot2::ggplot(aes(
+        xstart = start,
+        xend = end,
+        y = transcript_name
+    ))
+
+##### geom_junction_label_repel #####
+
+testthat::test_that(
+    "geom_junction() works correctly",
+    {
+        base_geom_junction_labels <- test_introns_plot +
+            geom_junction() +
+            geom_junction_label_repel(aes(label = count))
+        w_param_geom_junction_labels <- test_introns_plot +
+            geom_junction(
+                junction.y.max = 0.5
+            ) +
+            geom_junction_label_repel(
+                aes(label = count),
+                junction.y.max = 0.5
+            )
+        w_aes_geom_junction_labels <- test_introns_plot +
+            geom_junction(aes(colour = transcript_name)) +
+            geom_junction_label_repel(
+                aes(
+                    label = count,
+                    colour = transcript_name
+                )
+            )
+        w_facet_geom_junction_labels <- test_introns_plot +
+            geom_junction() +
+            geom_junction_label_repel(
+                aes(label = count)
+            ) +
+            ggplot2::facet_wrap(transcript_name ~ ., drop = TRUE)
+
+        vdiffr::expect_doppelganger(
+            "Base geom_junction_label_repel plot",
+            base_geom_junction_labels
+        )
+        vdiffr::expect_doppelganger(
+            "With param geom_junction_label_repel plot",
+            w_param_geom_junction_labels
+        )
+        vdiffr::expect_doppelganger(
+            "With aes geom_junction_label_repel plot",
+            w_aes_geom_junction_labels
+        )
+        vdiffr::expect_doppelganger(
+            "With facet geom_junction_label_repel plot",
+            w_facet_geom_junction_labels
+        )
+    }
+)
