c901d4b9f4d60d3309b61d2bbeaad6f4d63133a2
David Zhang <dyzhang32@gmail.com>
2022-02-19 23:00:26 +0000

    feat: forgot to commit script for generation of example junctions

--- data-raw/sod1_junctions.R
+++ data-raw/sod1_junctions.R
@@ -9,7 +9,7 @@ library(SummarizedExperiment)
 # Main --------------------------------------------------------------------
 
 # obtain gtex junctions across SOD1
-sod1_query <- snapcount::QueryBuilder(compilation="gtex", regions = "SOD1")
+sod1_query <- snapcount::QueryBuilder(compilation = "gtex", regions = "SOD1")
 
 # keeping only unannotated junctions
 # from liver where SOD1 is highly expressed
@@ -21,34 +21,34 @@ sod1_junctions <- snapcount::query_jx(sod1_query)
 
 # obtain mean counts
 mean_counts <-
-  sod1_junctions %>%
-  SummarizedExperiment::assays() %>%
-  .[["counts"]] %>%
-  as.matrix() %>%
-  rowMeans()
+    sod1_junctions %>%
+    SummarizedExperiment::assays() %>%
+    .[["counts"]] %>%
+    as.matrix() %>%
+    rowMeans()
 
 sod1_junctions <- sod1_junctions %>%
-  SummarizedExperiment::rowRanges() %>%
-  as.data.frame() %>%
-  dplyr::as_tibble()
+    SummarizedExperiment::rowRanges() %>%
+    as.data.frame() %>%
+    dplyr::as_tibble()
 
 # minor QC and tidying of the junctions
 sod1_junctions <-
-  sod1_junctions %>%
-  dplyr::mutate(mean_count = mean_counts) %>%
-  dplyr::filter(mean_count > 0.3) %>%
-  dplyr::select(
-    seqnames,
-    start,
-    end,
-    strand,
-    mean_count
-  )
+    sod1_junctions %>%
+    dplyr::mutate(mean_count = mean_counts) %>%
+    dplyr::filter(mean_count > 0.3) %>%
+    dplyr::select(
+        seqnames,
+        start,
+        end,
+        strand,
+        mean_count
+    )
 
 # Save data ---------------------------------------------------------------
 
 usethis::use_data(
-  sod1_junctions,
-  compress = "gzip",
-  overwrite = TRUE
+    sod1_junctions,
+    compress = "gzip",
+    overwrite = TRUE
 )
