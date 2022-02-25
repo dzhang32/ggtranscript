2cb24a8a1d9d887de6b570b45b193efb0c8dd70f
David Zhang <dyzhang32@gmail.com>
2022-02-25 21:49:39 +0000

    test(add_utr): add test for add_utr() with shorten_gaps() (#5)

--- tests/testthat/test-add_utr.R
+++ tests/testthat/test-add_utr.R
@@ -75,3 +75,95 @@ testthat::test_that(
         ))
     }
 )
+
+##### add_utr & shorten_gaps #####
+
+pknox1_cds_utr_rescaled <-
+    shorten_gaps(
+        exons = pknox1_cds_utr,
+        introns = to_intron(pknox1_cds_utr, "transcript_name"),
+        group_var = "transcript_name"
+    )
+
+# add labels helps manual checking
+plot_before_after_rescaled <- function(cds_utr_before,
+                                       cds_utr_after,
+                                       group_var,
+                                       add_labels = FALSE) {
+    before_rescaling <- cds_utr_before %>%
+        dplyr::filter(type == "CDS") %>%
+        ggplot2::ggplot(ggplot2::aes_string(
+            xstart = "start",
+            xend = "end",
+            y = group_var
+        )) +
+        geom_range() +
+        geom_range(
+            data = cds_utr_before %>% dplyr::filter(type == "UTR"),
+            height = 0.25,
+            fill = "white"
+        ) +
+        geom_intron(
+            data = to_intron(cds_utr_before, "transcript_name"),
+        )
+
+    after_rescaling <- cds_utr_after %>%
+        dplyr::filter(type == "CDS") %>%
+        ggplot2::ggplot(ggplot2::aes_string(
+            xstart = "start",
+            xend = "end",
+            y = group_var
+        )) +
+        geom_range() +
+        geom_range(
+            data = cds_utr_after %>% dplyr::filter(type == "UTR"),
+            height = 0.25,
+            fill = "white"
+        ) +
+        geom_intron(
+            data = to_intron(
+                cds_utr_after %>%
+                    dplyr::filter(type != "intron"),
+                "transcript_name"
+            ),
+        )
+
+    before_after_list <- list(before_rescaling, after_rescaling)
+
+    if (add_labels) {
+        for (i in seq_len(length(before_after_list))) {
+            before_after_list[[i]] <- before_after_list[[i]] +
+                ggrepel::geom_label_repel(
+                    ggplot2::aes_string(
+                        x = "start",
+                        y = group_var,
+                        label = "start"
+                    ),
+                    min.segment.length = 0
+                )
+        }
+    }
+
+    before_after_plot <- ggpubr::ggarrange(
+        plotlist = before_after_list, nrow = 2
+    )
+
+    return(before_after_plot)
+}
+
+testthat::test_that(
+    "shorten_gaps works correctly",
+    {
+        test_rescaled_w_utr_plot <- plot_before_after_rescaled(
+            pknox1_cds_utr,
+            pknox1_cds_utr_rescaled,
+            group_var = "transcript_name",
+            add_labels = FALSE
+        )
+
+        vdiffr::expect_doppelganger(
+            "test rescaled with utr plot",
+            test_rescaled_w_utr_plot
+        )
+    }
+)
